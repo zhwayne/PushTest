83474a55ec691db0b6357fca72b4055de2c57c91
zhwayne <mrzhwayne@163.com>
2026-02-28 09:48:01 +0800

    Initial Commit

---
+++ PushTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PushTest
+//
+//  Created by zw on 2026/2/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PushTest/PushTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushTestApp.swift
+//  PushTest
+//
+//  Created by zw on 2026/2/28.
+//
+
+import SwiftUI
+
+@main
+struct PushTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
